cbfdcb6c9b979bfcd11b96c9890da4fd51a9986c
Quocdat1205 <quocdatsc1205@gmail.com>
2022-06-01 09:25:44 +0700

    init repo

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import * as ReactDOM from "react-dom";
+import { createGlobalStyle } from "styled-components";
+
+import App from "./App";
+import { globalStyle } from "./styles";
+const GlobalStyle = createGlobalStyle`
+  ${globalStyle}
+`;
+
+// @ts-ignore
+declare global {
+  // tslint:disable-next-line
+  interface Window {
+    web3: any;
+    ethereum: any;
+    Web3Modal: any;
+    [name: string]: any;
+  }
+}
+
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById("root")
+);
